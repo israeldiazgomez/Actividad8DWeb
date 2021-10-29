ddd030162a8b22f9754ed38d02ee349558d166db
YilberthAndres <yilberthgalarza444@gmail.com>
2021-10-28 22:21:06 -0500

    yilberth

--- src/controllers/cliente.controllers.ts
+++ src/controllers/cliente.controllers.ts
@@ -19,7 +19,29 @@ export class ClienteController {
         }
     }
 
+    public async crearClientes(req: Request, res:Response){
+        const body: ClienteI = req.body;
+        
+        try {
+            if(!body.nombre && !body.apellido && 
+            !body.correo && !body.clave) return res.status(400).json({msg: "Requieren datos!!"});
     
+                const clienteExist: Cliente | null = await Cliente.findOne(
+                    {
+                        where: {correo: body.correo}
+                    }
+                );
+            if(clienteExist){
+                return res.status(400).json({msg: "Correo ya existe!!"})
+            }
+            const cliente = await Cliente.create(body);
+            res.status(200).json({cliente})
+    
+        } catch (error) {
+            res.status(500).json({msg: "Error al crear cliente"})
+        }
+    
+    }
 
 
 
