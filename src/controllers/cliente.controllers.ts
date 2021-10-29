be243bb3dfbd19c98546290f59cc7526b3ae588e
YilberthAndres <yilberthgalarza444@gmail.com>
2021-10-28 22:18:16 -0500

    yilberth

--- src/controllers/cliente.controllers.ts
+++ src/controllers/cliente.controllers.ts
@@ -3,3 +3,24 @@ import { Request, response, Response } from "express";
 import { Cliente } from "../models/Cliente";
 import { ClienteI } from "../models/Cliente";
 
+export class ClienteController {
+
+    public async getClientes(req: Request, res: Response){
+    
+        try {
+            const clientes = await Cliente.findAll()
+            if(!clientes){
+                res.status(400).json({msg: 'Cliente invalido'})
+            }
+    
+            return res.status(200).json({clientes})
+        } catch (error) {
+            res.status(500).json({msg: "Sin conexion con la base de datos"})
+        }
+    }
+
+    
+
+
+
+}
