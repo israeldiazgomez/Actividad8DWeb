6efa6e3ee540578d6d4441ac4c2739d03d8ba9f0
leinys <lepagifo@hotmail.com>
2021-10-28 22:49:30 -0500

    leinys

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,13 @@
 import { MACADDR } from 'sequelize/types';
-
+import {ProductoRoutes} from './producto';
+import {ProveedoreRoutes} from './provedoree';
 
 export class Routes {
+    public proveedoreRoutes: ProveedoreRoutes = new ProveedoreRoutes();
+    public distribuidoRoutes: DistribuidoRoutes = new DistribuidoRoutes();
 
 }
+
+
+
+
