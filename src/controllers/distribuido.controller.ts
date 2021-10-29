5fe896223e41ee77289bbd979e336000642de537
leinys <lepagifo@hotmail.com>
2021-10-28 22:12:33 -0500

    leinys

--- src/controllers/distribuido.controller.ts
+++ src/controllers/distribuido.controller.ts
@@ -25,4 +25,84 @@ export class DistribuidoController{
     public mostrarDistribuidoss(){
         
     }
-}
+    public async crearDistribuido(req:Request, res:Response){
+        // const body: ClienteI = req.body 
+        const {
+             id,
+            cantidad,
+             estado
+        } = req.body 
+
+        try {
+
+            let body : DistribuidoI = {
+                cantidad,
+                estado
+               
+            }
+            if((!body.cantidad)) return res.status(400).json({msg:'ingrese datos'});
+            const dis : Distribuido | null = await Distribuido.findOne({where: { cantidad : body.cantidad}});
+
+            const distribuido = await Distribuido.create(body);
+            res.status(200).json({distribuido});
+
+        } catch (error){
+    
+        }
+        
+    }
+    public async updateDistribuido(req: Request, res:Response){
+        const {id : pk} = req.params;
+        const {
+            id,
+            cantidad,
+            estado
+       } = req.body 
+
+      try {
+          
+        let body : DistribuidoI = {
+            cantidad,
+            estado
+        }
+        const DistribuidoExist: DistribuidoI | null = await Distribuido.findByPk(pk)
+        if (!DistribuidoExist)return res.status(500).json({msg: ' no existe'})
+
+        await Distribuido.update(
+            body,
+            {
+              where: {id: pk}  
+            }
+
+           
+        )
+        const distribuido: DistribuidoI | null = await Distribuido.findByPk(pk)
+        res.status(200).json({distribuido})  
+      } catch (error) {
+          return res.status(500).json({msg : 'error internal'})
+      }
+    }
+
+        public async deleteDistribuido(req: Request, res: Response){
+          const {id:pk}= req.params;
+          try {
+              const DistribuidoExist: DistribuidoI | null = await Distribuido.findByPk(pk);
+
+            if (!DistribuidoExist) return res.status(500).json({msg: ' no existe'})
+
+            await Distribuido.update(
+                {
+                    estado:"Desactivado"
+                },
+                    {
+                        where: {id:pk}
+                    }
+                
+            );
+            return res.status(200).json({msg: ' fue eliminado'})
+
+          } catch (error) {
+              
+          }
+      }
+    }
