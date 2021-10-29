bb09b23cf8ea4a2696c8cae186e3ad9045e361a8
YilberthAndres <yilberthgalarza444@gmail.com>
2021-10-28 22:06:34 -0500

    yilberth

---
+++ src/models/Cliente.ts
@@ -0,0 +1,11 @@
+import { database} from '../database/db';
+import { Model, DataTypes } from 'sequelize';
+
+
+export class Cliente extends Model {
+    public nombre!: string;
+    public apellido!: string;
+    public correo!: string;
+    public clave!: string;
+}
+
