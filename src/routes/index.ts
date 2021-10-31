06b60b65d697a49aa4713d3f6ffaa76636e6a716
Marlon-Alarcon <mralarcon@uniguajira.edu.co>
2021-10-31 15:16:22 -0500

    marlon

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,12 +4,14 @@ import { ProveedoreRoutes } from './proveedore';
 import { DistribuidoRoutes } from './distribuido';
 import { ProductoRoutes } from './producto';
 import { ClienteRoutes } from './cliente';
+import { CompraRoutes } from './compra';
 
 export class Routes {
     public proveedoreRoutes: ProveedoreRoutes = new ProveedoreRoutes();
     public distribuidoRoutes: DistribuidoRoutes = new DistribuidoRoutes();
     public productoRoutes: ProductoRoutes = new ProductoRoutes();
     public clientesRoutes: ClienteRoutes  = new ClienteRoutes();
+    public compraRoutes: CompraRoutes = new CompraRoutes();
 }
 
 
